96e53d6b6a5fa9a1242317904e277698f2477409
baddesign123 <baddesignhaiti@gmail.com>
2023-11-10 12:47:45 -0500

    Commit la

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Slugify',
+    version='0.1',
+    author=['Marcklens Badio', 'Wendel Hector'],
+    author_email=['baddesignhaiti@gmail.com', 'hectorwendel33@gmail.com'],
+    long_description='Trans+.',
+    url='https://github.com/wendoohector/slugify_projet.git',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
