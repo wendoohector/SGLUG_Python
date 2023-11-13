99a535c4a76cf807095befdc33a0dc4db2288d1d
wendel <wendelhector33@gmail.com>
2023-11-13 15:00:02 -0500

    Pati pam nan

---
+++ pack/doublon.py
@@ -0,0 +1,10 @@
+def retire_doublon(chen,ranplasman):
+    i=0
+    while i<len(chen)-1:
+        chen_list = list(chen)
+        if chen[i]==ranplasman and chen[i-1]==ranplasman:            
+            chen_list.pop(i)
+            chen = ''.join(chen_list)
+            i=0     
+        i+=1
+    return chen
--- pack/slugify.py
+++ pack/slugify.py
@@ -1,9 +1,11 @@
+from doublon import retire_doublon
 def slugify(chen, ramplasman="-"):
     lis="abcdefghijklmnopqrstuvwxyz1234567890-"
     chen=chen.lower()
     for element in chen:
         if element not in lis:
             chen=chen.replace(element,ramplasman)
+    chen=retire_doublon(chen, ramplasman)
     if chen[-1]==ramplasman:
         chen=chen[:-1]
     return chen
